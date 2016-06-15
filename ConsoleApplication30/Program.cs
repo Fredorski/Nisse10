1cd146319983e07e7b4669d355b15329d96d50fd
Freddie Wahlgren <freddie.wahlgren@gmail.com>
2016-06-15 14:13:42 +0200

    Nisse*10

--- ConsoleApplication30/Program.cs
+++ ConsoleApplication30/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApplication30
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Nisse");
+            }
         }
     }
 }
